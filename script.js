4fd0f81563c54fdf310ad396c0bd919260bce639
MM work <djjean90@gmail.com>
2021-10-08 18:36:46 +0300

    final

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //Script.js
+const bg = document.querySelector('.bg'),
+      text = document.querySelector('.loading-text');
+
+      let load=0;
+
+      const int = setInterval(bluring,30);
+
+function bluring() {
+    load++;
+
+    if(load>=100){
+        clearInterval(int);
+    }
+    text.textContent = `${load}%`;
+    bg.style.filter = `blur(${scale(load, 0, 100, 40, 0)}px)`;
+    text.style.opacity = scale(load, 0, 100, 1, 0);
+}
+
+
+const scale =function (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+};
